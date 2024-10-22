937c16993d8b997ec9d4f87de3ecc1e26149ac74
kailiseabrook <kailiseabrook@lcad.edu>
2024-10-22 08:22:40 -0700

    midterm
    finished midterm

---
+++ Assets/Scripts/Week6/Week6Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Week6Player : MonoBehaviour
+{
+    public bool hasRedKey = false;
+    public bool hasBlueKey = false;
+    public bool hasGreenKey = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+}
